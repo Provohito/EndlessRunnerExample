dee47c31a62554e2446aac129262fadfe1bdfd4b
Alex Semchenkov <zigola12@list.ru>
2021-08-26 17:56:35 +0300

    Implemented player movement
    The movement of the player along the Y axis in compliance with the restrictions on the limits of the map

---
+++ Assets/SimpleTemplate/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _stepSize;
+    [SerializeField] private float _MaxHeight;
+    [SerializeField] private float _MinHeight;
+
+
+    private Vector3 _targetPosition;
+
+    private void Start()
+    {
+        _targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (_targetPosition != transform.position)
+            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _moveSpeed * Time.deltaTime);
+    }
+
+    public void TryMoveUp()
+    {
+        if (_targetPosition.y < _MaxHeight)
+            SetNextPosition(_stepSize);
+    }
+
+    public void TryMoveDown()
+    {
+        if(_targetPosition.y > _MinHeight)
+            SetNextPosition(-_stepSize);
+    }
+
+    private void SetNextPosition(float stepSize)
+    {
+        _targetPosition = new Vector2(_targetPosition.x, _targetPosition.y + stepSize);
+    }
+}
