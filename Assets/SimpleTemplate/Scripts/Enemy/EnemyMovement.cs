a4a084d9c335df765578c3733153aa255e74a490
Alex Semchenkov <zigola12@list.ru>
2021-08-30 12:13:14 +0300

    Create Enemy scripting
    Create some mechanic of enemy(Movement and when enemy die)

---
+++ Assets/SimpleTemplate/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+    }
+}
